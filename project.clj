993f2cac8f1ca508142b1ed32c7083c67f7407c2
jsulmont <modality@gmail.com>
2020-11-23 08:35:23 +0100

    kafka 2.6.0 + others

--- project.clj
+++ project.clj
@@ -8,24 +8,24 @@
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]]
 
   :dependencies [[aleph "0.4.6"]
-                 [clj-time "0.15.1"]
+                 [clj-time "0.15.2"]
                  [danlentz/clj-uuid "0.1.9"
                   :exclusions [primitive-math]]
                  ;; Confluent does paired releases with Kafka, this should tie
                  ;; off with the kafka version.
                  ;; See https://docs.confluent.io/current/release-notes.html
 
-                 [io.confluent/kafka-schema-registry-client "5.3.1"
+                 [io.confluent/kafka-schema-registry-client "6.0.0"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
-                 [io.confluent/kafka-avro-serializer "5.3.1"]
-                 [org.apache.kafka/kafka-clients "2.3.1"]
-                 [org.apache.kafka/kafka-streams "2.3.1"]
-                 [org.apache.kafka/kafka-streams-test-utils "2.3.1"]
+                 [io.confluent/kafka-avro-serializer "6.0.0"]
+                 [org.apache.kafka/kafka-clients "2.6.0"]
+                 [org.apache.kafka/kafka-streams "2.6.0"]
+                 [org.apache.kafka/kafka-streams-test-utils "2.6.0"]
                  [org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.fressian "0.2.1"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.clojure/core.cache "0.7.2"]]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/data.fressian "1.0.0"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/core.cache "1.0.207"]]
 
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}
   :aot [jackdaw.serdes.edn2 jackdaw.serdes.fressian jackdaw.serdes.fn-impl]
@@ -70,13 +70,13 @@
               :resource-paths ["test/resources"]
               :injections [(require 'io.aviso.logging.setup)]
               :dependencies [[io.aviso/logging "0.3.2"]
-                             [org.apache.kafka/kafka-streams-test-utils "2.3.1"]
-                             [org.apache.kafka/kafka-clients "2.3.1" :classifier "test"]
-                             [org.clojure/test.check "0.9.0"]
-                             [org.apache.kafka/kafka_2.11 "2.3.1"]
-                             [lambdaisland/kaocha "0.0-529"]
-                             [lambdaisland/kaocha-cloverage "0.0-32"]
-                             [lambdaisland/kaocha-junit-xml "0.0-70"]]}
+                             [org.apache.kafka/kafka-streams-test-utils "2.6.0"]
+                             [org.apache.kafka/kafka-clients "2.6.0" :classifier "test"]
+                             [org.clojure/test.check "1.1.0"]
+                             [org.apache.kafka/kafka_2.11 "2.4.1"]
+                             [lambdaisland/kaocha "1.0.700"]
+                             [lambdaisland/kaocha-cloverage "1.0.75"]
+                             [lambdaisland/kaocha-junit-xml "0.0.76"]]}
 
              ;; This is not in fact what lein defines repl to be
              :repl
